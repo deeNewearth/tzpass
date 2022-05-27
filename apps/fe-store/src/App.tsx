1d55723f71ea1fac912c02e7601e0abaaa851bf5
dee acharjya <deeAcharjya@gamestop.com>
2022-05-27 09:18:53 -0600

    mono repo working

--- apps/fe-store/src/App.tsx
+++ apps/fe-store/src/App.tsx
@@ -3,9 +3,11 @@ import logo from './logo.svg';
 import './App.scss';
 import MainNav from './components/nav';
 import { Routes, Route, Link } from "react-router-dom";
+import {hello} from "@newearth/fe-comps/build/connect";
 
 
 function App() {
+  const h = hello();
   return (
     <div className="App">
       <MainNav/>
---
+++ packages/component-a/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+
+export const ComponentA: React.SFC<{}> = props =>
+  <div>ComponentA  3.1</div>
